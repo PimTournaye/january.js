7971548b60b7456fa77d6f11e6b8d6b299241e2c
Pim Tournaye <pim.tournaye@student.ehb.be>
2021-10-12 15:44:08 +0200

    typing fixes + hopefully play fucntion fix

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,22 @@ import Action from './actions/action';
 import { randomNumber } from './utils/randomNumber';
 import vamp from './actions/vamp.action';
 import harmony from './actions/harmony.action';
+import note from './logic/Note';
 
-const five: any = require('johnny-five');
-const pixel: any = require('node-pixel')
+//const five: any = require('johnny-five');
+//const pixel: any = require('node-pixel')
 
 
 //Pinout init
 //Pinouts are bound to change
-
+/* 
 const SMALL_BUTTON_1= five.Button(2);
 const SMALL_BUTTON_2 = five.Button(3);
 const SMALL_BUTTON_3 = five.Button(4);
 
 const OCTAVE_BUTTON = five.Button(5);
 const VAMP_BUTTON = five.Button(6);
-const CHROD_BUTTON = five.Button(7);
+const CHORD_BUTTON = five.Button(7);
 
 const HARMONY_BUTTON = five.Button(8);
 const TRANSPOSE_BUTTON = five.Button(9);
@@ -34,7 +35,7 @@ const PIXEL_RING_SMALL_PIN = "AO";
 const PIXEL_RING_BIG_CHORDS_PIN = null;
 const PIXEL_RING_BIG_HARMONY_PIN = null;
 const PIXEL_RING_BIG_TRANSPOSE_PIN = null;
-
+ */
 // NeoPixel sticks and rings
 let pixelSticks
 let smallRings;
@@ -65,8 +66,8 @@ let specialsAreUnlocked: boolean = false;
 
 // MAIN
 
-let board = new five.Board();
-
+//let board = new five.Board();
+/* 
 board.on("ready", () => {
 
 
@@ -151,50 +152,51 @@ board.on("ready", () => {
 
     // Buttons init
 
-    smallButton1.on("down", smallAction());
-    smallButton2.on("down", smallAction());
-    smallButton3.on("down", smallAction());
+    SMALL_BUTTON_1.on("down", smallAction());
+    SMALL_BUTTON_2.on("down", smallAction());
+    SMALL_BUTTON_3.on("down", smallAction());
 
-    octaveButton.on("down", () => {
+    OCTAVE_BUTTON.on("down", () => {
         console.log("Octave notes");
         octave.onPress();
         smallScore();
     })
     
-    vampButton.on("down", () => {
+    VAMP_BUTTON.on("down", () => {
         console.log("Vamp chord");
         vamp.onPress();
         chordScore();
     })
 
-    chordButton.on("down", () => {
+    CHORD_BUTTON.on("down", () => {
         console.log("Vamp chord");
         vamp.onPress();
         chordScore();
     })
 
-    harmonyButton.on("down", () => {
+    HARMONY_BUTTON.on("down", () => {
         console.log("Harmony action");
         harmony.onPress();
         specialScore();
     })
 
-    transposeButton.on("down", () => {
+    TRANSPOSE_BUTTON.on("down", () => {
         console.log("Transposed!");
         transpose.onPress();
         specialScore();
     })
 
-});
+}); */
 
 
 
-/* 
-const ACTIONS_NAMES_ARRAY: Array<Action> = [small,chord,transpose,octave]; //small ,chord,transpose,octave
+
+const ACTIONS_NAMES_ARRAY: Array<Action> = [small,octave, vamp, chord,harmony, transpose]; //small ,chord,transpose,octave
 main()
    
 async function main(){
     mode.init();
+    note.lastRecorded = "C3"
 
     for await (const action of ACTIONS_NAMES_ARRAY) {
         console.log(action.toString() + " is playing")
@@ -202,4 +204,3 @@ async function main(){
         await delay(3000);
     }
 }
- */
